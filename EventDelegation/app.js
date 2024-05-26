2d1cb55a325cdba712506f1494a00698adac5427
JustinLi3 <Triamethyest@gmail.com>
2024-05-26 13:31:35 -0400

    testing

--- EventDelegation/app.js
+++ EventDelegation/app.js
@@ -4,7 +4,7 @@ console.log(target);
 if(target.matches("button")){  
     target.style.color = 'Black' ; 
     target.innerHTML = "CLICKED";    
-    target.classList.remove('bg-info'); 
+    target.classList.remove('bg-info');  //Must remove background color class to set new color
     target.classList.remove('bg-warning');
     target.classList.remove('bg-success');
     target.classList.remove('bg-danger');
